65c432be31f4d2074d2b90599442527f7dd2c947
hhdhwuus <david.dengle02@gmail.com>
2024-07-26 19:18:45 +0200

    chore: Refactor photo upload logic and improve camera button functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -199,11 +199,50 @@
 		currentElement = null;
 		currentPhoto = null;
 	}
+
+	async function addPhoto() {
+			const image = await Camera.getPhoto({
+				quality: 90,
+				allowEditing: false,
+				resultType: CameraResultType.Uri
+			});
+
+			let imageUrl = image.webPath;
+			if (!imageUrl) {
+				return;
+			}
+			photos.update((currentPhotos) => {
+				let photo = {
+					id: crypto.randomUUID(),
+					date: new Date(),
+					url: imageUrl,
+					loaded: false
+				};
+				let img = new Image();
+				img.src = photo.url;
+				img.onload = async () => {
+					photos.update((currentPhotos) => {
+						let updatedPhoto = currentPhotos.find((p) => p.id === photo.id);
+						if (updatedPhoto) {
+							updatedPhoto.loaded = true;
+						}
+						return currentPhotos;
+					});
+				};
+				return [...currentPhotos, photo, photo, photo, photo, photo, photo, photo];
+			});
+		}
 </script>
 
 <div class="fullscreen-overlay" bind:this={fullscreenOverlay}>
 	<div class="container">
-		<h4>{currentPhoto?.date.toLocaleDateString(navigator.language, {day: "numeric", month: "long", year: "numeric"})}</h4>
+		<h4>
+			{currentPhoto?.date.toLocaleDateString(navigator.language, {
+				day: 'numeric',
+				month: 'long',
+				year: 'numeric'
+			})}
+		</h4>
 		<div class="buttons">
 			<button>
 				<Trash2 strokeWidth="1.5" size="20" />
@@ -215,6 +254,11 @@
 	</div>
 </div>
 
+<ion-header translucent>
+	<ion-toolbar>
+		<ion-title>Photos</ion-title>
+	</ion-toolbar>
+</ion-header>
 
 <ion-content fullscreen bind:this={content}>
 	<div class="photo-grid">
@@ -241,46 +285,9 @@
 			</div>
 		{/each}
 	</div>
-	<button class="camera-button"
-		on:click={async () => {
-			const image = await Camera.getPhoto({
-				quality: 90,
-				allowEditing: false,
-				resultType: CameraResultType.DataUrl
-			});
-
-			// image.webPath will contain a path that can be set as an image src.
-			// You can access the original file using image.path, which can be
-			// passed to the Filesystem API to read the raw data of the image,
-			// if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-			let imageUrl = image.dataUrl;
-			console.log(imageUrl);
-			if (!imageUrl) {
-				return;
-			}
-			photos.update((currentPhotos) => {
-				let photo = {
-					id: crypto.randomUUID(),
-					date: new Date(),
-					url: imageUrl,
-					loaded: false
-				};
-				let img = new Image();
-				img.src = photo.url;
-				img.onload = async () => {
-					photos.update((currentPhotos) => {
-						let updatedPhoto = currentPhotos.find((p) => p.id === photo.id);
-						if (updatedPhoto) {
-							updatedPhoto.loaded = true;
-						}
-						return currentPhotos;
-					});
-					console.log('loaded ' + photo.loaded);
-				};
-				return [...currentPhotos, photo];
-			});
-			console.log(photos);
-		}}
+	<button
+		class="camera-button"
+		on:click={addPhoto}
 	>
 		<CameraIcon />
 	</button>
@@ -334,6 +341,7 @@
 		pointer-events: none;
 		transition-duration: 0.3s;
 		transition-timing-function: cubic-bezier(0.1, 0, 0.15, 1);
+		padding: var(--ion-safe-area-top) 0 var(--ion-safe-area-bottom) 0;
 	}
 
 	.fullscreen-overlay .buttons {
@@ -372,10 +380,10 @@
 
 	.camera-button {
 		position: fixed;
-		bottom: 16px;
+		bottom: calc(16px + var(--ion-safe-area-bottom));
 		right: 16px;
-		width: 56px;
-		height: 56px;
+		width: 64px;
+		height: 64px;
 		border-radius: 50%;
 		z-index: 10;
 	}
